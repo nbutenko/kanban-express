3a47ddc290f87fda76dff12ccbc49184fabb2dc7
nbutenko <nadea.butenko@gmail.com>
2021-05-10 23:57:34 -0500

    User Model fix

--- modules/user/Model.js
+++ modules/user/Model.js
@@ -11,7 +11,7 @@ const userSchema = new Schema( {
         type: String,
         required: true,
         index: true,
-        select: false,
+        // select: false,
     },
     firstName: {
         type: String,
