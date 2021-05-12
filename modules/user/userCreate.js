38f710cb159b8c6d90e2e74a569e03152edb874c
nbutenko <nadea.butenko@gmail.com>
2021-05-11 19:50:55 -0500

    User Component fix

--- modules/user/userCreate.js
+++ modules/user/userCreate.js
@@ -15,7 +15,7 @@ function userCreate(req, res) {
         })
         .catch((err) => {
             console.log(err);
-            res.status(400).json(req.body.firstName + " " + req.body.lastName);
+            res.status(400).json("User not created");
         })
         .finally(() => {
             console.log("END");
